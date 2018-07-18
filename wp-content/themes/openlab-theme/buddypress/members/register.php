350d80afb58b5eb1d12172137754f89100ea42b7
Boone B Gorges <boonebgorges@gmail.com>
2018-07-18 13:58:57 -0500

    Change openlab-theme to a module.

--- wp-content/themes/openlab-theme/buddypress/members/register.php
+++
@@ -1,9 +0,0 @@
-<?php /**
- *  sign up form template
- *
- */
-?>
-
-<div class="col-sm-18">
-	<?php openlab_registration_page(); ?>
-</div><!--content-->
--- wp-content/themes/openlab-theme/readme.md
+++
@@ -1,3 +0,0 @@
-WordPress theme for Commons In A Box - OpenLab project
-
-Not ready for use on production sites.
