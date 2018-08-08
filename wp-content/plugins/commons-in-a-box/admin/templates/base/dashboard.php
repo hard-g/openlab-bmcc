4b6e30e8b39f85de22dff7ba378d1b50e3fa297a
Boone B Gorges <boonebgorges@gmail.com>
2018-08-08 09:48:08 -0500

    Remove commons-in-a-box in favor of a module.

--- wp-content/plugins/commons-in-a-box/admin/templates/base/dashboard.php
+++
@@ -1,4 +0,0 @@
-
-		<?php cbox_get_template_part( 'welcome' ); ?>
-
-		<?php cbox_get_template_part( 'getting-started' ); ?>
--- wp-content/plugins/commons-in-a-box/admin/templates/classic/description.php
+++
@@ -1,4 +0,0 @@
-
-<p>Available since v1.0, this is the original configuration of Commons In A Box.</p>
-
-<p>Suitable for small communities and working groups.</p>
