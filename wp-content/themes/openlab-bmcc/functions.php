3cefaa550341fab9ec4b1ac5c68b3459d0c89bed
Boone B Gorges <boonebgorges@gmail.com>
2018-09-17 17:37:15 +0000

    Link color update.

--- wp-content/themes/openlab-bmcc/functions.php
+++ wp-content/themes/openlab-bmcc/functions.php
@@ -1,6 +1,8 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', function() {
+	wp_enqueue_style( 'openlab-bmcc', get_stylesheet_directory_uri() . '/assets/css/openlab.css' );
+
 	wp_enqueue_script( 'openlab-bmcc-registration', get_stylesheet_directory_uri() . '/assets/js/register.js', array( 'openlab-registration' ) );
 
 	if ( bp_is_register_page() && ! empty( $_GET['account-key'] ) ) {
